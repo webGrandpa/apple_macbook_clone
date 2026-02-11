265f4f1f55380ac68850e0edc1ef5f80a2afb616
giorgi khiladze <goga.khilo@gmail.com>
2026-02-11 14:41:04 +0400

    added custom styles in header and hero sections

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,36 @@
 const Hero = () => {
   return (
-    <section id="hero">
-        <div>
-            <h1>MacBook Pro</h1>
-            <img src="/public/title.png" alt="MacBook Title" />
-            <video src="/videos/hero.mp4" autoPlay muted playsInline ></video>
-            <button>
+    <section 
+    className="h-fit lg:h-[80vh] col-center mt-40 lg:mt-20 2xl:mt-[7vh]"
+    id="hero">
+        <div
+        className="relative z-10 col-center lg:translate-y-16"
+        >
+            <h1
+            className="font-semibold lg:text-3xl 2xl:text-5xl text-white"
+            >MacBook Pro</h1>
+        <div className="relative text-center container mx-auto w-2/3 whitespace-nowrap">
+            <h1 
+                className="absolute inset-0 flex items-center justify-center text-2xl md:text-7xl bg-clip-text text-transparent bg-gradient-to-r from-cyan-500 via-purple-500 to-orange-500 blur-xl opacity-70 select-none" 
+                aria-hidden="true"
+            >
+                Built for Apple Intelligence.
+            </h1>
+            <h1 className="relative text-2xl md:text-7xl bg-clip-text text-transparent bg-gradient-to-r from-cyan-300 via-purple-300 to-orange-300 drop-shadow-[0_0_15px_rgba(255,255,255,0.7)]">
+                Built for Apple Intelligence.
+            </h1>
+        </div>
+            <video 
+            className="mx-auto lg:translate-y-5 2xl:translate-y-0"
+            src="/videos/hero.mp4" autoPlay muted playsInline ></video>
+            <button
+            className="relative z-10 bg-primary text-white py-2 px-6 mt-10 lg:mt-0 mb-5 rounded-full font-semibold text-lg cursor-pointer hover:bg-white hover:text-black transition-all duration-300 ease-in-out"
+            >
                 Buy
             </button>
-            <p>From $1599 or 133$/mo for 12 months</p>
+            <p
+            className="lg:h3-semibold base-semibold"
+            >From $1599 or 133$/mo for 12 months</p>
         </div>
     </section>
   )
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,25 +2,35 @@ import { navLinks } from '../constants'
 
 const NavBar = () => {
   return (
-    <header>
-        <nav>
-            <img src="/logo.svg" alt="Apple logo" />
+    <header className='w-screen fixed top-0 left-0 z-50 flex-center bg-black min-h-[7vh]'>
+        <nav className='container mx-auto flex-between px-5 2xl:px-0'>
+            <img 
+            className='cursor-pointer hover:-translate-y-0.5 transition-all duration-300 ease-in-out'
+            src="/logo.svg" alt="Apple logo" />
 
-            <ul>
+            <ul className='flex-center gap-8'>
                 {
                     navLinks.map(({label}) => (
                         <li key={label}>
-                            <a href={label}>{label}</a>
+                            <a 
+                            className='hidden md:block text-white opacity-80 font-regular text-sm cursor-pointer hover:opacity-100 transition-all duration-300 ease-in-out'
+                            href={label}>{label}</a>
                         </li>
                     ))
                 }
             </ul>
             <div className="flex-center gap-3">
-                <button>
-                    <img src="/search.svg" alt="search" />
+                <button 
+                className='bg-transparent border-none outline-none cursor-pointer'>
+                    <img 
+                    className='cursor-pointer hover:-translate-y-0.5 transition-all duration-300 ease-in-out'
+                    src="/search.svg" alt="search" />
                 </button>
-                <button>
-                    <img src="/cart.svg" alt="cart" />
+                <button 
+                className='bg-transparent border-none outline-none cursor-pointer'>
+                    <img 
+                    className='cursor-pointer hover:-translate-y-0.5 transition-all duration-300 ease-in-out'
+                    src="/cart.svg" alt="cart" />
                 </button>
             </div>
         </nav>
