73cf050b060aa679f883a5832c2f95905d50674f
giorgi khiladze <goga.khilo@gmail.com>
2026-02-11 13:37:10 +0400

    Implement Hero section with video animation

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+
 import NavBar from './components/NavBar'
+import Hero from './components/Hero'
 
 const app = () => {
   return (
     <main>
         <NavBar />
+        <Hero />
     </main>
   )
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+const Hero = () => {
+  return (
+    <section id="hero">
+        <div>
+            <h1>MacBook Pro</h1>
+            <img src="/public/title.png" alt="MacBook Title" />
+            <video src="/videos/hero.mp4" autoPlay muted playsInline ></video>
+            <button>
+                Buy
+            </button>
+            <p>From $1599 or 133$/mo for 12 months</p>
+        </div>
+    </section>
+  )
+}
+
+export default Hero
